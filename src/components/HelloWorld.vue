e7a23669b5ee53c9f444a2c6b8d5edb0a08319be
avil13 <13.avil@gmail.com>
2019-10-31 23:12:52 +0300

    add vue-sweetalert2 plugin

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,132 +1,86 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div id="app" class="container">
+    <h1 class="h1">{{ msg }}</h1>
+    <h3>{{ description }}</h3>
+
+    <br />
+
+    <div class="row">
+      <button
+        @click="simple"
+        class="btn btn-outline-primary col s12 m3"
+        type="button"
+      >
+        simple
+      </button>
+      <button
+        @click="success"
+        class="btn btn-outline-primary col s12 m3"
+        type="button"
+      >
+        success
+      </button>
+      <button
+        @click="error"
+        class="btn btn-outline-primary col s12 m3"
+        type="button"
+      >
+        error
+      </button>
+      <button
+        @click="toastTopEnd"
+        class="btn btn-outline-primary col s12 m3"
+        type="button"
+      >
+        toast top end
+      </button>
+    </div>
+
+    <a href="https://sweetalert2.github.io" class="doc-link" target="_blank"
+      >Sweetalert2 documentation</a
+    >
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { Component, Vue } from "vue-property-decorator";
 
-export default Vue.extend({
-  name: "HelloWorld",
-  props: {
-    msg: String
+@Component
+export default class AppComponent extends Vue {
+  msg = "Welcome to Vue-Sweetalert2 example";
+  description = "This is HelloWorld TypeScript component";
+
+  simple() {
+    this.$swal("Hello world!");
   }
-});
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+  success() {
+    Vue.swal({
+      type: "success",
+      title: "Hello",
+      text: "Hello brave new world!"
+    });
+  }
+
+  error() {
+    this.$swal({
+      type: "error",
+      title: "Oops...",
+      text: "Something went wrong!"
+    });
+  }
+
+  toastTopEnd() {
+    this.$swal({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+
+      type: "success",
+      title: "Hi man",
+      text: "is a good day!"
+    });
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+</script>
